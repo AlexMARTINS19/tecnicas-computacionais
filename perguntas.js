828945c662706c3013abd56f06e3c0915e98a540
AlexMARTINS19 <117647365+AlexMARTINS19@users.noreply.github.com>
2024-08-30 20:20:13 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,113 +1,113 @@
 export const perguntas = [
     {
-        enunciado: "1. Como você prefere passar seu tempo livre?",
+        enunciado: "1. Qual tipo de carro você prefere dirigir?",
         alternativas: [
             {
-                texto: "A) Lendo um livro",
+                texto: "A) Fusca",
                 afirmacao: [
-                    "O Pensador. Você aprecia momentos de introspecção e adora mergulhar em novos mundos através da leitura. Sua natureza reflexiva e curiosa faz de você uma pessoa que valoriza o conhecimento e a tranquilidade."
+                    "O Nostálgico. Você é alguém que valoriza as tradições e aprecia as coisas simples da vida. Prefere a segurança do que já conhece e gosta de reviver momentos do passado com um toque de saudade."
                 ],
                 proxima: 1,
             },
             {
-                texto: "B) Socializando com amigos",
+                texto: "B) Chevette",
                 afirmacao: [
-                    "O Extrovertido. Você se sente energizado quando está rodeado por outras pessoas. Adora compartilhar experiências, histórias e risadas, sendo alguém que valoriza as conexões e as amizades."
+                    "O Aventureiro. Você é uma pessoa que gosta de desafios e está sempre pronta para novas experiências. Adora explorar caminhos diferentes e não tem medo de tentar algo fora do comum."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "2. Qual estilo de viagem você prefere?",
+        enunciado: "2. Qual é o seu lugar preferido para dirigir?",
         alternativas: [
             {
-                texto: "Aventura na natureza",
+                texto: "Cidade",
                 afirmacao: [
-                    "O Explorador. Você adora o desconhecido e sente-se revigorado ao explorar o ar livre. Para você, as melhores experiências vêm da conexão com a natureza e da superação de desafios.",
+                    "O Urbano. Você se sente em casa no meio do trânsito e da agitação das ruas. Gosta do ritmo acelerado da cidade e se orgulha de ser capaz de enfrentar qualquer congestionamento com calma e determinação.",
     
                 ],
                 proxima: 3,
             },
             {
-                texto: "Cidades históricas",
+                texto: "Estrada",
                 afirmacao: [
-                    "O Curioso Cultural. Você é fascinado por história, arte e arquitetura. Prefere viagens que ofereçam riqueza cultural e um mergulho profundo nas tradições e histórias locais."
+                    "O Explorador. Você prefere pegar a estrada e sentir o vento no rosto. Para você, dirigir é mais sobre a jornada do que o destino, e o caminho aberto representa liberdade e novas possibilidades."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Qual é o seu ambiente de trabalho ideal?",
+        enunciado: "Qual o tipo de manutenção que você prefere?",
         alternativas: [
             {
-                texto: "Trabalhando sozinho em silêncio",
+                texto: "Manutenção Preventiva",
                 afirmacao: [
-                    "O Independente. Você valoriza a concentração e a autonomia no trabalho. Prefere ambientes tranquilos onde pode se dedicar totalmente às suas tarefas, sem interrupções."
+                    "O Precavido. Você gosta de manter tudo em ordem e evita problemas antes que eles aconteçam. Prefere seguir as regras e manter uma rotina, garantindo que tudo funcione perfeitamente."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Colaborando em equipe",
+                texto: "Manutenção Paliativa",
                 afirmacao: [
-                    "O Colaborativo. Você acredita que as melhores ideias surgem da troca de opiniões. Prefere trabalhar em equipe, onde pode compartilhar e receber feedback constante."
+                    "O Improvisador. Você é mais prático e resolve os problemas à medida que surgem. Não se preocupa tanto com o futuro e prefere lidar com as situações conforme aparecem, usando criatividade para resolver os desafios."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Como você lida com desafios?",
+        enunciado: "Qual o estilo de carroceria que mais combina com você?",
         alternativas: [
             {
-                texto: "Planejando cuidadosamente",
+                texto: "Sedan",
                 afirmacao: [
-                    "O Estrategista. Antes de agir, você prefere analisar todas as possibilidades. Sua abordagem meticulosa garante que você esteja preparado para qualquer eventualidade."
+                    "O Elegante. Você preza por conforto e estabilidade, gosta de impressionar com classe e prefere um estilo de vida organizado. Para você, a estética e a funcionalidade caminham lado a lado."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Agindo por impulso",
+                texto: "Picape",
                 afirmacao: [
-                    "O Intuitivo. Você confia na sua intuição e prefere agir rapidamente diante de desafios. Acredita que as melhores soluções muitas vezes vêm de decisões espontâneas."
+                    "O Prático. Você é direto ao ponto e gosta de versatilidade. Prefere um veículo que aguente o tranco e seja útil para todas as situações, seja para trabalho ou lazer."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Qual tipo de filme você prefere?",
+        enunciado: "Qual é sua relação com o consumo de combustível?",
         alternativas: [
             {
-                texto: "Drama",
+                texto: "Econômico",
                 afirmacao: [
-                    "O Sensível. Você aprecia histórias profundas que exploram a complexidade das emoções humanas. Para você, filmes são uma forma de se conectar com sentimentos intensos e narrativas significativas."
+                    "O Eficiente. Você prefere praticidade e está sempre pensando no custo-benefício. Valoriza a eficiência em tudo o que faz, e na estrada, isso se reflete em escolher veículos que não pesem no bolso."
                 ],
                 proxima: 7,
             },
             {
-                texto: "Comédia",
+                texto: "Desempenho",
                 afirmacao: [
-                    "O Alegre. Você prefere filmes leves e divertidos, que trazem alegria e riso. Acredita que o humor é uma das melhores formas de lidar com a vida e seus desafios."
+                    "O Potente. Você prioriza a potência e a velocidade, e não se importa de gastar um pouco mais para ter a experiência completa. Para você, o prazer de dirigir é mais importante do que o custo."
                 ],
                 proxima: 8,
             },
         ]
     },
     {
-        enunciado: "Como você prefere começar o dia?",
+        enunciado: "Qual é a sua relação com a velocidade?",
         alternativas: [
             {
-                texto: "Com uma rotina matinal estabelecida",
+                texto: "Moderada",
                 afirmacao: [
-                    "O Organizado. Você gosta de começar o dia com estrutura e previsibilidade. Para você, uma manhã bem planejada é o segredo para um dia produtivo."
+                    "O Cauteloso. Você prefere manter tudo sob controle e dirigir com segurança. Gosta de manter um ritmo constante e evita correr riscos desnecessários. Para você, o importante é chegar com tranquilidade."
                 ],
             },
             {
-                texto: "De forma espontânea e sem horários fixos",
+                texto: "Alta",
                 afirmacao: [
-                    "O Espontâneo. Você prefere a liberdade de seguir seu próprio ritmo. Acredita que cada dia deve ser vivido de acordo com o que o momento pede, sem muita rigidez."
+                    "O Acelerado. Você é apaixonado por adrenalina e não perde uma oportunidade de pisar fundo. Gosta de sentir a velocidade e o poder do carro, e para você, dirigir é uma forma de liberar toda a sua energia."
                 ],
             },
         ]
